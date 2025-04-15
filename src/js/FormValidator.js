3b36d90f400b1f4f5df1027dbdabe3afd57bee74
daniel hodgson <45772010+DanielCHodgson@users.noreply.github.com>
2025-04-15 20:13:04 +0100

    Added field highlights on invalid

--- src/js/FormValidator.js
+++ src/js/FormValidator.js
@@ -4,6 +4,7 @@ export default class FormValidator {
 
     if (input.validity.valid) {
       this.#clearError(errorContainer);
+      input.classList.remove("invalid");
       return true;
     } else {
       if (input.validity.valueMissing) {
@@ -11,33 +12,39 @@ export default class FormValidator {
       } else if (input.validity.typeMismatch) {
         this.#showError(errorContainer, "Please enter a valid email.");
       }
+      input.classList.add("invalid");
       return false;
     }
   }
 
   static validatePassword(input) {
     const errorContainer = input.nextElementSibling;
-  
+
     if (input.validity.valid && input.value.length >= 8) {
+      input.classList.remove("invalid");
       this.#clearError(errorContainer);
       return true;
+    } else if (input.validity.valueMissing) {
+      this.#showError(errorContainer, "Email is required.");
     } else {
       this.#showError(
         errorContainer,
         "Password must be at least 8 characters and include one uppercase letter, one lowercase letter, and one number."
       );
+      input.classList.add("invalid");
       return false;
     }
   }
-  
 
   static validateConfirmPassword(input, isMatch) {
     const errorContainer = input.nextElementSibling;
     if (isMatch) {
       this.#clearError(errorContainer);
+      input.classList.remove("invalid");
       return true;
     } else {
       this.#showError(errorContainer, "Passwords do not match.");
+      input.classList.add("invalid");
       return false;
     }
   }
@@ -45,9 +52,11 @@ export default class FormValidator {
   static validateCountry(input) {
     const errorContainer = input.nextElementSibling;
     if (input.validity.valid && input.value !== "") {
+      input.classList.remove("invalid");
       this.#clearError(errorContainer);
       return true;
     } else {
+      input.classList.add("invalid");
       this.#showError(errorContainer, "Please select a valid country.");
       return false;
     }
@@ -76,9 +85,13 @@ export default class FormValidator {
 
     if (pattern.test(input.value)) {
       this.#clearError(errorContainer);
+      input.classList.remove("invalid");
       return true;
     } else {
+      
+      country === "" ? this.#showError(errorContainer, "Select a country then enter a valid postcode") :
       this.#showError(errorContainer, `Enter a valid postcode for ${country}.`);
+      input.classList.add("invalid");
       return false;
     }
   }
